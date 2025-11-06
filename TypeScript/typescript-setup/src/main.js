aa5147e2169709fa67b9eb1bfc4484ce23a905dc
jek9356 <devjakariya9356@gmail.com>
2025-11-07 05:22:17 +0600

    create typescript project by tsc init

---
+++ TypeScript/typescript-setup/src/main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const username = "jakariya Ahmed";
+console.log(username);
+//# sourceMappingURL=main.js.map
---
+++ TypeScript/typescript-setup/src/main.ts
@@ -0,0 +1,13 @@
+const users:object = {
+    name: "Aman",
+    age: 26,
+    desi: "Developer"
+}
+
+console.log(users);
+
+
+const age:number = 26;
+console.log(age);
+
+console.log("Welcome to TypeScript");

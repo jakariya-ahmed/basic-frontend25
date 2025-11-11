27b12734049bc141b51efe710cf5231a02166a17
jek9356 <devjakariya9356@gmail.com>
2025-11-11 23:28:10 +0600

    operators

---
+++ JS25/JSInterview/basic/operators.js
@@ -0,0 +1,74 @@
+/* There are 9 Operators In JavaScript
+1. Arithmetic Operator   2. Assignment Operators   3. Comparison Operators
+4. Logical Operators  5. Bitwise Operators  6. String Operators 
+7. Ternary Operators  8. Type Operators  9. Spread & Rest Operators 
+
+*/
+
+
+/** 
+ ** 1. Arithmetic Operators **
+ *  Additon(+), Subtraction(-), Multiplication(*), Division(/), Modulus/Remainder(%)
+ *  Exponentiation/Power(**), Increment(++), Decrement(--),
+ */
+
+
+
+ /**
+  ** 2. Assigment Operators **
+  * Assign(=), Add and Assign(+=), Subtract and Assign(-=), Multiply and Assign(*=)
+  * Divide and Assign(/=), Modulus and Assign(%=), Power and Assign(**=)
+  */
+
+// Example:
+let sum = 10;
+    sum+=5;  // sum = sum + 5;
+    
+    let total = 40;
+    let price = 120;
+    total+=price;
+    
+    // console.log(total);
+
+
+/** 
+ ** 3.Comparision Operators  **
+ *  Equal to(5 == "5"), Equal to & same type strict(5 === "5"), Not equal(5 != "5"), Not equal & not same type(5 !== "5")
+ *  Greater than(8 > 5), Less than(3 < 7), Greater or Equal(5 >= 5), Less or Equal(4 <= 6)
+ */
+
+
+
+ /**
+  ** 3. Logical Operators **
+  * AND(5 > 3 && 2 < 4), NOT !(5 > 3)
+  */
+
+
+  /**
+   * 5. Bitwise Operators **
+   * And(&), OR(`), XOR(^), NOT(~), Left shift(<<), Right shift(>>)
+   */
+
+  /**
+   * 6. String Operators **
+   * Concatenation(+), Append(+=)
+   */
+
+/**
+ * 7. Ternary Operator / Conditional Operator
+ * condition ? expressionIfTrue : expressionIfFalse;
+ */
+
+
+/**
+ * 8. Type Operators
+ * typeof, instanceof,
+ */
+
+
+/**
+ * 9. Spread Operators
+ * spread(...), rest(...),
+ */
+

9271824562272ff0a22ec28126d77e49365c879a
jek9356 <devjakariya9356@gmail.com>
2026-07-01 20:13:12 +0600

    completed Reassignment variables declaration

--- JS25/BasicJS/variables-2.js
+++ JS25/BasicJS/variables-2.js
@@ -23,5 +23,25 @@ const isRemoteJob = true;
 
 
 
-/* Reassignment variables: */
+/************ Reassignment variables: *************/
+// let 
+let currentBalance = 1000;
+    currentBalance = 5000; // Reassigning the value of currentBalance
+
+// console.log("Current Balance:", currentBalance); // Output: Current Balance: 1000
+
+// var
+var previousBalance = 2000;
+    previousBalance = 3000; // Reassigning the value of previousBalanace
+// console.log("Previous Balance:", previousBalance); // Output: Previous Balance: 2000
+
+// const
+const favoriteColor = "Orange";
+    // favoriteColor = "Sky Blue"; //TypeError: Assignment to constant value of favoriteColor
+console.log("Favorite Color:", favoriteColor); // Output: Favorite Color: Orange
+
+
+/************ Reassignment variables: *************/
+
+
 

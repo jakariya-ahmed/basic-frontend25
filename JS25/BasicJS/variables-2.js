90e07e8ec2cb49e691e503f981b12e96d681c4e4
jek9356 <devjakariya9356@gmail.com>
2026-07-01 20:00:26 +0600

    variable types

---
+++ JS25/BasicJS/variables-2.js
@@ -0,0 +1,27 @@
+
+/** const fullName = "Jakariya Ahmed";
+const dateOfBirth = "2002-12-02";
+let age = 26;
+const favoriteLanguage = "JavaScript";
+const githubProfileUrl = "https://github.com/jakariyaahmed";
+let totalScore = 10;
+    totalScore += 5;
+const country = "Bangladesh";
+let isRemoteJobAvailable = true;
+*/
+
+
+const fullName = "Jakariya Ahmed";
+const dateOfBirth = new Date("2002-12-02");
+let currentAge = 26;
+const favoriteLanguage = "JavaScript";
+const githubProfileUrl = "https://github.com/jakariyaahmed";
+let totalScore = 10;
+    totalScore += 5;
+const country = "Bangladesh";
+const isRemoteJob = true;
+
+
+
+/* Reassignment variables: */
+

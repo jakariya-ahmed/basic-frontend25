58de73d83aedf829e64bacd3cdcb94dbf8b6c321
jek9356 <devjakariya9356@gmail.com>
2026-07-08 20:55:28 +0600

    arrray elements

--- JS25/Intermediate/array-methods.js
+++ JS25/Intermediate/array-methods.js
@@ -2,10 +2,10 @@
  * Arrays
 │
 ├── Basic Operations
-│   ├── push()
-│   ├── pop()
-│   ├── shift()
-│   ├── unshift()
+│   ├── push() -> Adds one or more elements to the end of and array and return the new length of array
+│   ├── pop() -> Removes the last element from an array and return that element
+│   ├── shift() -> Removes the first element form an array and return that element
+│   ├── unshift() -> Adds one or more element to the begining of an array and return the new length of array.
 │   ├── slice()
 │   ├── splice()
 │
@@ -49,18 +49,73 @@
  * 
  */
 
-const shoppingCart = ["Laptop", "Mouse"];
+const shoppingCart = [];
 
 /**
  * Add a newly selected product to the end of the cart
  * `push()` mutates te original array and return new length of array
  */
-
+shoppingCart.push("Laptop", "Mouse");
 shoppingCart.push("Keyboard");
 
 
+/**
+ * unshift() - Adds one or more elements to the begining of an array and return the new length of the array.
+ * Example:
+ * VIP Guest should be placed at the front of the sat
+ * `unshift()` mutates the original array and return new length of array
+ */
+
+const guestList = ["Jakariya", "Ahmed", "Ali"];
+
+/**
+ * Insert a VIP guest at the begining of the list
+ */
+
+guestList.unshift("Aman Khan");
+
+/**
+ * Why used const in array?
+ * In JavaScript, when you declare an array using const, it means that the reference to the array cannot be changed. 
+ * However, you can still modify the contents of the array (e.g., adding or removing elements) because the array itself is mutable.
+ * 
+ * In JavaScript, when an array refference is declared using const, it means that 
+ */
 
 
 
 
-console.log("Shopping Cart Products:", shoppingCart);
+/**
+ * pop() -> Removes the last elements from an array and returns that elements. 
+ * Example: 
+ * Remove the last product from the shopping cart
+ * `pop()` mutates the original array and return te removed elements
+ */
+
+const removedItem = shoppingCart.pop();
+
+
+
+
+/**
+ * shift() -> Removes the first element from an array and returns that element.
+ * Example: 
+ * Remove the first guest from the guest list
+ * `shift()` mutates the original array and return the removed elements
+ */
+
+
+const removedFirstGuest = guestList.shift();
+
+/** lenght();
+ * List of products currently in the customer's shopping cart
+ */
+const totalItem = shoppingCart.length;
+
+
+
+
+
+
+console.log("Total Products Cont:", totalItem);
+console.log("products:", shoppingCart);

c7d80e2244110ade29e91d057154a1a356347999
jek9356 <devjakariya9356@gmail.com>
2026-07-13 15:26:20 +0600

    entries and fromEntries object method practise

--- JS25/Intermediate/object.js
+++ JS25/Intermediate/object.js
@@ -66,12 +66,75 @@ const userVal = Object.values(user);
  * 
  */
 Object.entries(user).forEach(([key,val]) => {
-    console.log(`${key}: ${val}`);
+    // console.log(`${key}: ${val}`);
 });
 
 
+/**
+ * Object.entries() -> converts an object into an array of key-value paris
+ * 
+ */
+const entries = Object.entries(user);
 
-// console.log(user.name);/
+
+/**
+ * Invoice line item
+ */
+
+const invoice = {
+    id: 202,
+    laptop: 23393,
+    mouse: 203,
+    keboard: 430,
+}
+
+/**
+ * Print invoice
+ */
+
+Object.entries(invoice).forEach(([name, price]) => {
+    // console.log(`${name} - ${price}`);
+});
+
+
+/**
+ * fromEntries() -> does the opposite of entries()
+ */
+
+const entriesData = [
+    ['id', 105],
+    ['Name', "Jakariya"],
+    ['Role', "Frontedn Develooper"],
+    ['Status', 'Active'],
+];
+
+const entriesObject = Object.fromEntries(entriesData); 
+
+const activeEntries = Object.entries(entriesObject).filter(([key]) => {
+    return key !== 'Status';
+});
+
+
+const discountedPrice = Object.fromEntries(
+    Object.entries(invoice).map(([product, price]) => {
+        return [product, price * 0.9];
+    })
+);
+
+
+
+
+const employees = [
+    {id: 101, name: 'Jakariya Aman', department: 'Engineering', designation: 'Software Engineer', active: true},
+    {id: 102, name: 'Ali Aman', department: 'Design', designation: 'Graphic Designer', active: true},
+    {id: 103, name: 'Arif Akbar', department: 'Data Expert', designation: 'Data Analytics ', active: false},
+    {id: 104, name: 'Sayem Talukdar', department: 'Software Development', designation: 'Web Developer', active: true},
+    {id: 105, name: 'Mussadik ali', department: 'Engineering', designation: 'Software Engineer', active: false},
+];
+
+
+console.log(invoice);
+console.log(discountedPrice);
 
 
 
